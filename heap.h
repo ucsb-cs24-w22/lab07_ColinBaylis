0a2631cf56af9413e128250910a8aa6a5aadeb4b
ColinBaylis <91994288+ColinBaylis@users.noreply.github.com>
2022-03-09 20:10:26 -0800

    Update heap.h

--- heap.h
+++ heap.h
@@ -15,10 +15,10 @@ class Heap{
     void pop(); // delete the min element 
     int top();
     bool empty();
-    
+    void goUp(int index);
+    void goDown(int index);
  private:
     std::vector<int> vdata; //store the binary heap tree as a dynamic array
-    int min(int index) const;
 };
 
 #endif
